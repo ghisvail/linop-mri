80ffc221c5912788fb198f16da2305a47055679e
Ghislain Vaillant <ghisvail@gmail.com>
2013-11-29 16:58:11 +0000

    fix missing replace in docstring

--- tests/test_nfft.py
+++ tests/test_nfft.py
@@ -1,4 +1,4 @@
-"""Test suite for the linop_mri.fft module"""
+"""Test suite for the linop_mri.nfft module"""
 #linop-mri is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, either version 3 of the License, or
