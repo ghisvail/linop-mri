d253326582b3c1859d4d49e513024ca17093f7e7
Ghislain Vaillant <ghisvail@gmail.com>
2013-11-29 16:09:41 +0000

    add empty test suite

---
+++ tests/test_fft.py
@@ -0,0 +1,24 @@
+"""Test suite for the linop_mri.fft module"""
+#linop-mri is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#linop-mri is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with linop-mri.  If not, see <http://www.gnu.org/licenses/>.
+
+from numpy.testing import TestCase
+from linop_mri.fft import FFTOperator, FFTShiftOperator
+
+
+class TestFFTOperator(TestCase):
+    pass
+
+
+class TestFFTShiftOperator(TestCase):
+    pass
